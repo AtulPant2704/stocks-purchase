1f4116c954651a68c7435681c3d9d406f31b5b42
AtulPant2704 <atulpant1999@gmail.com>
2021-09-19 20:11:10 +0530

    added js

---
+++ app.js
@@ -0,0 +1,32 @@
+const initialPrice = document.querySelector("#initial-price");
+const currentPrice = document.querySelector("#current-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const submitBtn = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(stocksQuantity.value);
+    var curr = Number(currentPrice.value);
+    if(ip && qty && curr){
+        calculateProfitAndLoss(ip, qty, curr);
+    }else{
+        outputBox.innerText = `Please enter all the fields.`
+    }
+}
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
+        var loss = (initial - current) * quantity;
+        var lossPercentage = Number.parseFloat((loss / initial) * 100).toFixed(2);
+        outputBox.innerText = `Hey the loss is ${loss} and the loss percent is ${lossPercentage}%`;
+    } else if (current > initial) {
+        var profit = (current - initial) * quantity;
+        var profitPercentage = Number.parseFloat((profit / initial) * 100).toFixed(2);
+        outputBox.innerText = `Hey the profit is ${profit} and the profit percent is ${profitPercentage}%`;
+    } else {
+        outputBox.innerText = `No pain No gain, No gain No pain.`;
+    }
+}
