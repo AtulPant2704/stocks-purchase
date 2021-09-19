de10de1dc6e10a03f5ccf8cd45f81cd0d11444fc
AtulPant2704 <atulpant1999@gmail.com>
2021-09-19 20:42:07 +0530

    updated js styles

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
         var loss = (initial - current) * quantity;
         var lossPercentage = Number.parseFloat((loss / initial) * 100).toFixed(2);
-        outputBox.style.color= "green";
+        outputBox.style.color= "red";
         outputBox.innerText = `Hey the loss is ${loss} and the loss percent is ${lossPercentage}%`;
     } else if (current > initial) {
         var profit = (current - initial) * quantity;
         var profitPercentage = Number.parseFloat((profit / initial) * 100).toFixed(2);
-        outputBox.style.color= "red";
+        outputBox.style.color= "green";
         outputBox.innerText = `Hey the profit is ${profit} and the profit percent is ${profitPercentage}%`;
     } else {
         outputBox.innerText = `No pain No gain, No gain No pain.`;
